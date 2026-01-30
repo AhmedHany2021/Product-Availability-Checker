498fe946f4b056f6b0d305ec379e5503b4aab748
ahmedhanyshah <ahmed.hany@shahbandr.team>
2026-01-30 11:08:25 +0200

    add plugin header and main defines

---
+++ product-availability-checker.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: Product Availability Checker
+ * Plugin URI: https://github.com/AhmedHany2021/Product-Availability-Checker
+ * Description: Check product availability by zip code for WooCommerce.
+ * Version: 1.0.0
+ * Author: Ahmed Hany
+ * Author URI: https://github.com/AhmedHany2021
+ * Text Domain: product-availability-checker
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+define( 'PAC_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
+define( 'PAC_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
